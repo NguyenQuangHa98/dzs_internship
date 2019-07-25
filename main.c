0adeb94fff2344aab0aee62bbb0aa08e29f25dcf
nguyenquangha98 <dev10@gmail.com>
2019-07-25 12:26:26 +0900

    optimize code

--- main.c
+++ main.c
@@ -6,29 +6,31 @@
 #include <sys/time.h>
 #include <pthread.h>
 
-
 #include "thread_get_key.h"
 #include "thread_compare.h"
 #include "variable_get_char.h"
 
-void render_map(char *file_name);
-void stop_and_check(int level, int random_pos, int score_temp, time_t begin_time);
-
 int dead_time = 1;
 char evil_pos = 0;
 int counter = 0;
+
+void render_map(char *file_name);
+void wait_and_check(int level, int random_pos, int score_temp, time_t begin_time);
+void print_info(time_t begin_time);
+
 int main()
 {
 	int random_pos, score_temp;
 	char *file_name;
 	int level = 1;
+	time_t begin_time, elapsed_time;
 
 	/* create new thread to get key typed by player */
 	pthread_t keyboard;
 	pthread_create(&keyboard, NULL, thread_keyboard, NULL);
 
 	/* starting timer */
-	time_t begin_time = time(NULL);
+	begin_time = time(NULL);
 
 	while(1)
 	{	
@@ -37,17 +39,28 @@ int main()
 
 		/* print 9 empty boxes, player's score and time left */
 		render_map("textart");
-		printf("\n%d\n", counter);
-		printf("%d\n", 60 - (int)difftime(time(NULL), begin_time));
+
+		/* print scores and time left */
+		print_info(begin_time);
 
 		sleep(1);
 
 		/* calculate elapsed time */
-		if(difftime(time(NULL), begin_time) >= 60)
-		{	
-			// print something...
+		elapsed_time = time(NULL) - begin_time;
+		if(elapsed_time >= 60)
+		{
+			if(elapsed_time == 60)
+			{
+				render_map("textart");
+				print_info(begin_time);
+			}
+
+			/* print result (or record) */
+			printf("Timeout! Your scores: %d\n", counter);
+
 			dead_time = 0;
-			pthread_exit(NULL);
+			pthread_join(keyboard, NULL);
+			return 0;
 		}
 
 		/* print 1 evil and 8 empty boxes */
@@ -60,14 +73,15 @@ int main()
 		file_name = (char*)malloc(30 * sizeof(char));
 		snprintf(file_name, 30, "%s%d", "textart", random_pos);
 
-		/* get current score to check if it increase or not */
+		/* get current score to check if then it increase or not */
 		score_temp = counter;
 
-		/* get appropriate file and render to console*/
+		/* get appropriate file and print to console*/
 		render_map(file_name);
 		free(file_name);
-		printf("\n%d\n", counter);
-		printf("%d\n", 60 - (int)difftime(time(NULL), begin_time));
+
+		/* print scores and time left */
+		print_info(begin_time);
 
 		/* update current position of evil */
 		evil_pos = random_pos + 48;
@@ -75,11 +89,13 @@ int main()
 		/* update level based on scores that player achieved */
 		if(counter >= 5 && counter < 15 && level < 2)
 			level = 2;
-		else if(counter >= 15 && level < 3)
+		else if(counter >= 15 && counter < 25 && level < 3)
 			level = 3;
-		
-		//sleep_based_level(level);
-		stop_and_check(level, random_pos, score_temp, begin_time);
+		else if(counter >= 25 && level < 4)
+			level = 4;
+
+		/* wait for tapping from player and check that input */
+		wait_and_check(level, random_pos, score_temp, begin_time);
 	}
 	return 0;
 }
@@ -100,10 +116,10 @@ void render_map(char *file_name)
 		fclose(fp);
 }
 
-void stop_and_check(int level, int random_pos, int score_temp, time_t begin_time)
+void wait_and_check(int level, int random_pos, int score_temp, time_t begin_time)
 {
 	char file_name[30];
-	static int level_time[3] = {1500000, 1000000, 500000};
+	static int level_time[4] = {1500000, 1000000, 500000, 300000};
 	struct timeval start, current;
 	gettimeofday(&start, NULL);
 
@@ -112,6 +128,7 @@ void stop_and_check(int level, int random_pos, int score_temp, time_t begin_time
 		gettimeofday(&current, NULL);
 		if(current.tv_sec * 1000000 + current.tv_usec >= start.tv_sec * 1000000 + start.tv_usec + level_time[level - 1])
 			break;
+
 		if(counter > score_temp)
 		{
 			score_temp = counter;
@@ -120,8 +137,13 @@ void stop_and_check(int level, int random_pos, int score_temp, time_t begin_time
 			/* render map to screen */
 			render_map(file_name);
 			/* print score and time left to screen */
-			printf("\n%d\n", counter);
-			printf("%d\n", 60 - (int)difftime(time(NULL), begin_time));
+			print_info(begin_time);
 		}
 	}
+}
+
+void print_info(time_t begin_time)
+{
+	printf("\nScores: %d\n", counter);
+	printf("Time left: %ld\n", 60 - (time(NULL) - begin_time));
 }
